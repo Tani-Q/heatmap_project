970c36bf3ae5b3d13ec3f03af88f335a708e24ce
Tani-Q <tani_kyuichiro@nurve.jp>
2020-04-28 15:48:28 +0900

    GCFに関する開発コード

--- gz2json.py
+++ gz2json.py
@@ -1,27 +1,18 @@
 # -*- coding:utf-8 -*-
+
+#ライブラリ
 import sys
-import csv
-import gzip
-import json
-import datetime
-
-
-import os
-
-import pandas as pd 
 import google.cloud.storage
+import os
 from google.cloud import storage
-
+from google.cloud import storage as gcs
+import pandas as pd
+from io import BytesIO
 import re
+import json
+import tempfile
 
-import shutil
-
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/tani_kyuichiro/nurve-cloud-98b3f-bb7c97f8fb03.json'
-
-
-#------------------------------------ メソッド ------------------------------------------------
-
+#---------------------------- method --------------------------------------
 #GCSへのデータアップローダー    
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
         """Uploads a file to the bucket."""
@@ -41,8 +32,15 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
             )
         )
 
+"""def download_blob(bucket_name,file_name,project_name):
+    # create gcs cliaent
+    client = gcs.Client(project_name)
+    bucket = client.get_bucket(bucket_name)
+    # create blob
+    blob = gcs.Blob(file_name, bucket)
+    content = blob.download_as_string()
+    return pd.read_csv(BytesIO(content))"""
 
-#GCSからのデータダウンロード
 def download_blob(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
     # bucket_name = "your-bucket-name"
@@ -59,63 +57,93 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
         "Blob {} downloaded to {}.".format(
             source_blob_name, destination_file_name
         )
-    )
-#------------------------------------ メソッド ------------------------------------------------
+    ) 
 
-def main():
+ def loop_check(data):
+        print("data['name']:{}".format(data['name']))
+        if data['name'] == 'logging/heatmap.csv.gz':
+          return True  
+#---------------------------- method --------------------------------------
+
+def main(data,context):
+    print('--- Start!!--- ')
+    #無限ループ阻止
+    if not loop_check(data):
+     sys.exit(1)
+
+    #ディレクトリ準備
+    print('Dir作成')
+    tmpdir = tempfile.TemporaryDirectory()
+    dir_name = tmpdir.name
+
     #元データダウンロード
-    print('gzipファイルダウンロード中')
-    bucket_name = 'heatmap-staging'
-    source_blob_name = 'logging/heatmap.csv.gz'
-    destination_file_name = 'data/heatmap.csv.gz'
-    download_blob(bucket_name, source_blob_name, destination_file_name)
-    print('gzipファイルダウンロード完了')
+    #データダウンロード
+    print('ファイルダウンロード開始')
+    bucket_name = "heatmap-staging-csv"
+    #file_name = "logging/heatmap.csv"
+    project_name = "nurve-cloud-98b3f"
+    source_blob_name = "logging/heatmap.csv.gz"
+    file_name = dir_name + "/heatmap.csv.gz" 
+
+    #blobからjsonファイル取得
+    download_blob(bucket_name,source_blob_name,file_name)
+    #os.remove(file_name)
+    #print('ファイル削除')
+    #tmpdir.cleanup()
+    #print('Dir削除')
+    #df = download_blob(bucket_name,file_name,project_name)
+    
 
     #データ処理
     print('データ処理開始')
-    df = pd.read_csv('./data/heatmap.csv.gz', sep=',')
+    df = pd.read_csv(file_name, sep=',')
 
     media_id_lst = df['media_id'].values.tolist()
     unique_media_id_lst = list(set(media_id_lst))
 
     #media_id別データ選別
-    print('media_id別ファイル処理中')
-    for media_id in unique_media_id_lst:    
 
-        tmp1_dic = {}
-        tmp2_dic = {}
+    ##ディレクトリ準備
+    #tmpdir = tempfile.TemporaryDirectory()
+    #dir_name = tmpdir.name
+
+    print('media_id別ファイル処理中')
+    for media_id in unique_media_id_lst:
+        tmp_dic1 = {}
+        tmp_dic2 = {}
         tmp_lst = []
-       
+    
         #データ選別
         df_tmp = df[df['media_id'] == media_id ].reset_index(drop=True)
         
         media_id = str(int(df_tmp.iloc[0]['media_id']))
-        print('media_id:{}',format(media_id))
+        print('media_id:{}'.format(media_id))
         #tmp_dic['media_id'] =  int(df_tmp.iloc[0]['media_id'])
-        tmp1_dic['item_id'] = int(df_tmp.iloc[0]['item_id'])
-        tmp1_dic['organization_group_id'] = int(df_tmp.iloc[0]['organization_group_id'])
+        tmp_dic1['item_id'] = str(int(df_tmp.iloc[0]['item_id']))
+        tmp_dic1['organization_group_id'] = str(int(df_tmp.iloc[0]['organization_group_id']))
         #x,y座標データ取得
         xy_lst = df_tmp[['x','y']].values.tolist()
         for xy in xy_lst:
             tmp_lst.append(xy)
-        tmp1_dic['coordinate'] = tmp_lst
-        tmp2_dic[media_id] = tmp1_dic
-
+        tmp_dic1['coordinate'] = tmp_lst
+        tmp_dic2[media_id] = tmp_dic1
+    
         #json保存
-        savepath = './data/' + media_id + '.json'
+        savepath = dir_name + '/' + media_id + '.json'
+        #savepath = media_id + '.json'
         with open(savepath, 'w') as outfile:
-            json.dump(tmp1_dic, outfile)
+            json.dump(tmp_dic1, outfile)
         
         #GCSへアップロード
         bucket_name = 'heatmap-staging'
-        source_file_name = './data/' + media_id + '.json'
+        source_file_name = savepath
         destination_blob_name = 'medium_items/media/' + media_id + '/heatmap.json'
         upload_blob(bucket_name, source_file_name, destination_blob_name)
         print('upload完了')
         os.remove(source_file_name) #ローカルで作成したjsonファイル削除
-    #元データ削除
-    os.remove(destination_file_name)
-
+    #ディレクトリ解除
+    tmpdir.cleanup()
+    print('仮想ディレクトリ削除')
 
 if __name__ == '__main__':
     main()
--- heatmap_maker_json_gcf.py
+++ heatmap_maker_json_gcf.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 import sys
 import csv
+import gzip
+import json
 import math
 import datetime
 
@@ -23,13 +25,22 @@ import google.cloud.storage
 from google.cloud import storage
 
 import re
+
 import shutil
+
+import tempfile
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/tani_kyuichiro/nurve-cloud-98b3f-bb7c97f8fb03.json'
 
+#-------------------------------- メソッド -------------------------------------
 
+def json_paser(json):
+    json_open = open(json, 'r')
+    json_load = json.load(json_open)
 
-#---------------------------------- メソッド -------------------------------------------
+    print(json_load)
+    
+
 #座標補正メソッド
 def corrected_data(line):
     xy_lst = []
@@ -58,6 +69,36 @@ def corrected_data(line):
     else:
         xy_lst.append(fix_y)
     return xy_lst
+
+#json用座標補正メソッド
+def corrected_data_json(line):
+    xy_lst = []
+    #　小数点以下切り上げ
+    #line[1] = math.ceil(float(line[1]))
+    #line[2] = math.ceil(float(line[2]))
+    x = math.ceil(float(line[0]))
+    y = math.ceil(float(line[1]))
+    
+    #x軸補正
+    #if line[1] < 0:
+    if x < 0:    
+        #xy_lst.append(line[1]*(-1) + 90)
+        xy_lst.append(x*(-1) + 90)
+    else:
+        #xy_lst.append((line[1] - 90)*(-1))
+        xy_lst.append((x - 90)*(-1))
+     #補正無しX軸   
+    #xy_lst.append(line[1])
+    
+    #y軸補正
+    #fix_y = line[2] + 180
+    fix_y = y + 180
+    if fix_y > 360 :
+        xy_lst.append(fix_y - 360)
+    else:
+        xy_lst.append(fix_y)
+    return xy_lst
+
 
 #余白削除メソッド
 def margin_cut(file_name='./hm.jpg',output_name='./hm_edge.png'):
@@ -119,47 +160,14 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
         )
     ) 
 
-#item_idかmdia_idかで実行するsqlを変える
-def sql_selecter(c_type,num):
-    if c_type == 'item_id':
-        sql1 = "SELECT id as media_id, item_id ,x ,y FROM panorama_user_event t1 \
-        JOIN ( SELECT id, mediumable_id FROM nurvecloud.media WHERE mediumable_type = 'Item' ) t2 \
-        ON mediumable_id = item_id WHERE item_id ="
-        sql2 = " AND x IS NOT NULL AND y IS NOT NULL ORDER BY time"
-        sql = sql1 + num + sql2
-    else:
-        sql1 = "SELECT id as media_id, item_id ,x ,y FROM panorama_user_event t1 \
-        JOIN ( SELECT id, mediumable_id FROM nurvecloud.media WHERE mediumable_type = 'Item' ) t2 \
-        ON mediumable_id = item_id WHERE id ="
-        sql2 = " AND x IS NOT NULL AND y IS NOT NULL ORDER BY time"
-        sql = sql1 + num + sql2
-    return sql
 
-#item_idかmdia_idかで実行するsqlを変える上に実行日付から過去一週間分を収集する
-def sq_selecter_with_daterange(c_type,num):
-    dt_now = datetime.datetime.now()
-    today = dt_now.strftime("%Y-%m-%d")
-    if c_type == 'item_id':
-        sql1 = "SELECT id as media_id, item_id ,x ,y FROM panorama_user_event t1 \
-        JOIN ( SELECT id, mediumable_id FROM nurvecloud.media WHERE mediumable_type = 'Item' ) t2 \
-        ON mediumable_id = item_id \
-        WHERE item_id = "
-        sql2 = " AND x IS NOT NULL AND y IS NOT NULL  AND \
-        TD_TIME_RANGE(t1.time, TD_TIME_ADD('"
-        sql3 = today + "','-7d','JST'),'"
-        sql4 = today + "', 'JST')"
-        sql = sql1 + num + sql2 + sql3 + sql4
-    else:
-        sql1 = "SELECT id as media_id, item_id ,x ,y FROM panorama_user_event t1 \
-        JOIN ( SELECT id, mediumable_id FROM nurvecloud.media WHERE mediumable_type = 'Item' ) t2 \
-        ON mediumable_id = item_id \
-        WHERE id = "
-        sql2 = " AND x IS NOT NULL AND y IS NOT NULL  AND \
-        TD_TIME_RANGE(t1.time, TD_TIME_ADD('"
-        sql3 = today + "','-7d','JST'),'"
-        sql4 = today + "', 'JST')"
-        sql = sql1 + num + sql2 + sql3 + sql4        
-    return sql    
+#データ量計測
+def data_len(*lst):
+    lines = list(lst) #list形式に変換一次
+    #print(type(lines))
+    line_s = lines[0] #list形式に変換二次
+    return len(line_s)
+        
 
 #ヒートマップデータ作成
 def data_maker(*axis_data_lst):
@@ -240,28 +248,44 @@ def extension_get(bucket_name,prefix):
     else:
         return False
 
+#バケット内のオブジェクトリスト取得    
+def list_blobs(bucket_name):
+        """Lists all the blobs in the bucket."""
+        # bucket_name = "your-bucket-name"
 
-# main処理
-def heatmap_maker(val,id):
-    #-----------------------------------  SQL処理  -------------------------------------------------
-    #TDから直接座標データ吸い取り
-    print('TDからデータ取り込み開始')
-    # Initialize query engine
-    engine = td.create_engine('presto:rails_events_production')
+        storage_client = storage.Client()
 
-    # Read Treasure Data query into a DataFrame.
-    sql = sql_selecter(val,id) #全データ
-    #sql = sq_selecter_with_daterange(val,id) #期間指定
-    df = td.read_td(sql, engine)
-    lines = df.values.tolist()
-    print('データ取り込み完了')
+        # Note: Client.list_blobs requires at least package version 1.17.0.
+            
+        bucket = storage_client.get_bucket('rent-production')
+        blobs = bucket.list_blobs(prefix="medium_items/media/", delimiter="/")
+        dirs = []
+        for page in blobs.pages:
+            dirs.extend(page.prefixes)
+
+        [ print(x) for x in dirs ]
+
+#jsonで処理するタイプ    
+def heatmap_maker_json(file,dir_name):
+    #-----------------------------------  json処理  -------------------------------------------------
+    
+    df_s = pd.read_json(file)
+
 
     #読み込み画像データ設定
+    num = re.search(r'\d+',file)
+    media_id = num.group()
+    item_id =  df_s.loc[0]['item_id']
+    organization_group_id =  df_s.loc[0]['organization_group_id']
+    lines =  df_s['coordinate'].values.tolist()
+    
+    
+    """#読み込み画像データ設定
     line = lines[0]
     file = str(int(line[1])) #item_id
-    media_id = str(int(line[0])) #media_id
+    media_id = str(int(line[0])) #media_id"""
 
-    print('item_id:{}/media_id:{}'.format(file,media_id))
+    print('item_id:{}/media_id:{}'.format(item_id,media_id))
 
     #------------------------------ データ作成 -------------------------------------
     #座標補正処理 Ver02
@@ -269,7 +293,7 @@ def heatmap_maker(val,id):
     f_lst = []
     idx = 0
     for line in lines:    
-        data = corrected_data(line) #座標補正
+        data = corrected_data_json(line) #座標補正
         axis_data_lst.append(data)
     print('座標補正完了')
 
@@ -284,7 +308,7 @@ def heatmap_maker(val,id):
 
 
     #凡例付きヒートマップ作成
-    hm_name_sample = "./tmp/hm_org.jpg"
+    hm_name_sample = dir_name + "/hm_org.jpg"
     plt.figure(figsize=(40,16)) 
     #fig, ax = plt.subplots(figsize=(32,16))
     plt.tick_params(labelsize=30)
@@ -294,14 +318,14 @@ def heatmap_maker(val,id):
     print('凡例付きヒートマップ作成完了')
 
     #合成用ヒートマップ作成
-    hm_name = "./tmp/hm.jpg" #<- dirをつける
+    hm_name = dir_name + "/hm.png" #<- dirをつける
     plt.figure(figsize=(32,16)) 
     sns.heatmap(lst_2d,vmin=0, vmax=v_max,yticklabels=False,xticklabels=False,cbar=False)
-    plt.savefig(hm_name )
+    plt.savefig(hm_name)
     print('合成用ヒートマップ作成完了')
 
     #HM余白削除
-    outfile = './tmp/hm_edge.jpg'
+    outfile = dir_name + '/hm_edge.png'
     #outfile = margin_cut(hm_name,outfile)
     margin_cut(hm_name,outfile)
     print('余白削除処理完了')
@@ -316,73 +340,67 @@ def heatmap_maker(val,id):
     prefix = 'medium_items/media/'+media_id + '/'
     ext = extension_get(bucketname,prefix) #拡張子吸い出し
     if not ext:
-        #exit() 
-        return False #画像ファイルが無ければ終了
+        return False
     
     #bucket_name = 'rent-production/medium_items/media/'+media_id
     bucket_name = 'rent-production'
     source_blob_name = 'medium_items/media/'+media_id + '/2048x1024.' + ext
-    file_name = './imgdata/2048x1024.jpg'
+    file_name = dir_name + '/2048x1024.jpg'
     try:
         download_blob(bucket_name,source_blob_name,file_name)
     except:
         print('file not found. {}'.format(source_blob_name))
+        return False
         
     print('オリジナル画像読み込み完了')
 
     #----------------------------------------- 画像処理 ------------------------------------------------------------
     #画像データ読み込み
-    filename = './imgdata/2048x1024.jpg'
+    filename = dir_name + '/2048x1024.jpg'
     try:
         img = cv2.imread(filename)
         height = img.shape[0] # Errorを引っ掛けるためだけの仕掛
     except  AttributeError:
         print('file not found. {}'.format(filename))
         return False
-    
-    #サイズが異なる画像が登録されていた場合処理を閉める
-    if img.shape[0] == 1024 and img.shape[1] == 2048:
-        print('img:{}'.format(img.shape))
-    else:
-        print('size is diff!')
-        return False
 
     #余白除去後HM読み込み
-    filename = './tmp/hm_edge.jpg' # <- dirをつける
+    filename = dir_name + '/hm_edge.png' # <- dirをつける
     try: 
-        img2 = cv2.imread(filename)
+        img2 = cv2.imread(filename,-1)
         height = img2.shape[0] # Errorを引っ掛けるためだけの仕掛
     except  AttributeError:
         print('file not found.')
-        return False
 
     #サイズ調整
     height = img2.shape[0]
     width = img2.shape[1]
 
-    #img2_1 = cv2.resize(img2 , (int(width*(2048.5/width)), int(height*(1024/height))))
-    img2_1 = cv2.resize(img2 , (int(width*(2048/width)), int(height*(1024/height))))
-    print('img2_1:{}'.format(img2_1.shape))
+    img2_1 = cv2.resize(img2 , (int(width*(2048.5/width)), int(height*(1024/height))))
+    #print(img2_1.shape)
+    cv2.imwrite(dir_name + '/resize_heatmap.png', img2_1)
     print('ヒートマップのサイズ調整完了')
 
     #画像合成
     blended = cv2.addWeighted(src1=img,alpha=0.6,src2=img2_1,beta=0.4,gamma=0.3)
 
     #合成後画像保存
-    cv2.imwrite('./tmp/blended_test.jpg', blended) # <- dirをつける
+    cv2.imwrite(dir_name + '/blended_test.jpg', blended) # <- dirをつける
     print('合成画像作成完了')
 
+    #半透明画像作成
+    im_rgb = Image.open(dir_name + '/resize_heatmap.png')
+    im_rgb.putalpha(128)
+    im_rgb.save(dir_name + '/pillow_putalpha_solid.png')
 
     #凡例切り出し準備
     im = Image.open(hm_name_sample)
-    
 
     #凡例切り出し&resize
-    #im_crop = im.crop((2230,120,2350,1030))
-    im_crop = im.crop((3150,170,3250,1450)) #2020/04/16 凡例切り出しのズレが生じていたので修正
+    im_crop = im.crop((2230,120,2350,1030))
     im_crop_rsize = im_crop.resize((100,1024))   
     plt.imshow(im_crop_rsize)
-    im_crop_rsize.save('./tmp/colorbar_crop.jpg', quality=100) # <- dirをつける
+    im_crop_rsize.save(dir_name + '/colorbar_crop.jpg', quality=100) # <- dirをつける
     print('凡例作成完了')
 
 
@@ -392,14 +410,14 @@ def heatmap_maker(val,id):
     blank = np.zeros((height, width, 3))
     blank += 255 #←全ゼロデータに255を足してホワイトにする
  
-    cv2.imwrite('./tmp/blank.jpg',blank) # <- dirをつける
+    cv2.imwrite(dir_name + '/blank.jpg',blank) # <- dirをつける
     print('ベース画像作成完了')
 
 
     #凡例付きヒートマップ作成
-    img0 = cv2.imread('./tmp/blank.jpg') # ブランク画像
-    img1 = cv2.imread('./tmp/blended_test.jpg') # ヒートマップ合成画像
-    img2 = cv2.imread('./tmp/colorbar_crop.jpg') # 凡例
+    img0 = cv2.imread(dir_name + '/blank.jpg') # ブランク画像
+    img1 = cv2.imread(dir_name + '/blended_test.jpg') # ヒートマップ合成画像
+    img2 = cv2.imread(dir_name + '/colorbar_crop.jpg') # 凡例
 
     img0 = cv2.cvtColor(img0, cv2.COLOR_BGR2RGB)
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
@@ -416,90 +434,143 @@ def heatmap_maker(val,id):
     base_img[y_offset:y_offset+img1.shape[0], x_offset:x_offset+img1.shape[1]] = img1
 
     #凡例画像貼り付け基準点設定
-    #x_offset=2050
-    x_offset=2100
+    x_offset=2050
     y_offset=0
 
     #凡例画像合成
     base_img[y_offset:y_offset+img2.shape[0], x_offset:x_offset+img2.shape[1]] = img2
     base_img = cv2.cvtColor(base_img, cv2.COLOR_BGR2RGB)
 
-    media_dir = './medium_items/media/' + media_id 
+    #media_dir = './medium_items/media/' + media_id 
+    media_dir = dir_name + '/' + media_id + '.jpg'
 
-    if not os.path.exists(media_dir):
-        os.makedirs(media_dir) #dirが無い場合作成
+    """if not os.path.exists(media_dir):
+        os.makedirs(media_dir) #dirが無い場合作成"""
 
-    dir_file_name = './medium_items/media/' + media_id + '/heatmap.jpg'
+    #dir_file_name = './medium_items/media/' + media_id + '/heatmap.jpg'
 
-    cv2.imwrite(dir_file_name,base_img)
+    #cv2.imwrite(dir_file_name,base_img)
+    cv2.imwrite(media_dir,base_img)
     print('凡例付き合成画像作成完了')
+    
+    bucket_name = "heatmap-staging"
+    source_file_name = media_dir
+    destination_blob_name = 'medium_items/media/' + media_id + '/heatmap.jpg'
+    upload_blob(bucket_name, source_file_name, destination_blob_name) #画像データアップロード
+    
+    #作成画像削除
+    os.remove(dir_name + "/hm_org.jpg")
+    os.remove(dir_name + "/hm.png")
+    os.remove(dir_name + '/hm_edge.png')
+    os.remove(dir_name + '/2048x1024.jpg')
+    os.remove(dir_name + '/blended_test.jpg') 
+    os.remove(dir_name + '/resize_heatmap.png')
+    os.remove(dir_name + '/pillow_putalpha_solid.png')
+    os.remove(dir_name + '/colorbar_crop.jpg') 
+    os.remove(dir_name + '/blank.jpg')
+    os.remove(media_dir)
+    
 
-    #----------------------------------------- GCSへのアップロード -----------------------------------------------
-    # Create a storage client.
+    #return True    
+    
+#GCSへのデータアップローダー    
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+        """Uploads a file to the bucket."""
+        # bucket_name = "your-bucket-name"
+        # source_file_name = "local/path/to/file"
+        # destination_blob_name = "storage-object-name"
 
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+
+        blob.upload_from_filename(source_file_name)
+
+        print(
+            "File {} uploaded to {}.".format(
+                source_file_name, destination_blob_name
+            )
+        )
+
+def down_blob(bucket_name,source_blob_name,file_name):
+        #------------------------------------- GCSからの画像データ読み込み -------------------------------------------------
+    #直接画像読み込み
     #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/tani_kyuichiro/nurve-cloud-98b3f-bb7c97f8fb03.json'
-
     
-    storage_client = google.cloud.storage.Client()
-
-    bucket_name = 'rent-heatmap' 
-    bucket = storage_client.get_bucket(bucket_name)
-
-    source_file_name = 'medium_items/media/' + media_id + '/heatmap.jpg'
-    blob = bucket.blob(source_file_name)
- 
-    # Upload the local file to Cloud Storage.
-    blob.upload_from_filename(source_file_name)
- 
-    print('File {} uploaded to {}.'.format(source_file_name,bucket))
-    print('GCSへのアップロード完了')
-
-    #---------------------------------------------- ディレクトリ削除 -----------------------------------------------
-    shutil.rmtree('./medium_items/media/' + media_id)
-    print('dir削除')
     
-
-    return True
+    #ファイルの存在確認、拡張子取得
+    bucketname = 'rent-production'
+    prefix = 'medium_items/media/'+media_id + '/'
+    ext = extension_get(bucketname,prefix) #拡張子吸い出し
+    if not ext:
+        return False
     
-#---------------------------------- メソッド -------------------------------------------
-def main():
-    #コマンド入力
-    args = sys.argv
-    val = args[1]
-    id = str(args[2])
-    heatmap_maker(val,id)
-
-    #dt_now = datetime.datetime.now()
-    #today = dt_now.strftime("%Y-%m-%d")
+    #bucket_name = 'rent-production'
+    #source_blob_name = 'medium_items/media/'+media_id + '/2048x1024.' + ext
+    #file_name = './tmp/2048x1024.jpg'
+    try:
+        download_blob(bucket_name,source_blob_name,file_name)
+    except:
+        print('file not found. {}'.format(source_blob_name))
+        return False
+        
+    print('オリジナル画像読み込み完了')
     
-    #前日に参照されたitem画像の一週間のデータを元にヒートマップ画像を作成
-    """sql = "WITH base as (SELECT id as media_id,t1.item_id,mediumable_id,cnt \
-    ,TD_TIME_FORMAT(resent_time,'yyyy-MM-dd HH:mm:ss','JST') as jp_time \
-    FROM \
-    (SELECT item_id,COUNT(*) as cnt FROM panorama_user_event \
-    WHERE TD_TIME_RANGE(time, TD_TIME_ADD('" + today + "','-8d','JST'), '" + today + "', 'JST') \
-    GROUP BY item_id \
-    ORDER BY cnt DESC ) t1 \
-    JOIN \
-    (SELECT id, mediumable_id FROM nurvecloud.media WHERE mediumable_type = 'Item') t2 ON mediumable_id = item_id \
-    JOIN \
-    (SELECT item_id ,MAX(time) OVER(PARTITION BY item_id) as resent_time FROM panorama_user_event WHERE \
-    TD_TIME_RANGE(time, TD_TIME_ADD('" + today + "','-8d','JST'), '" + today + "', 'JST')) t3 \
-    ON t1.item_id = t3.item_id ORDER BY resent_time DESC, cnt DESC) \
-    SELECT DISTINCT * FROM base WHERE cnt > 10 limit 10
+#バケット内のオブジェクトリスト取得
+def list_blobs(bucket_name):
+        """Lists all the blobs in the bucket."""
+        # bucket_name = "your-bucket-name"
 
-    engine = td.create_engine('presto:rails_events_production')
+        storage_client = storage.Client()
 
-    # Read Treasure Data query into a DataFrame.
-    df2 = td.read_td(sql, engine)
-    lines2 = df2.values.tolist()
-    for line in lines2:
-        id = str(line[1])
-        check = heatmap_maker('item_id',id)
-        if not check:
-            continue"""
+        # Note: Client.list_blobs requires at least package version 1.17.0.
+        blobs = storage_client.list_blobs(bucket_name)
+        
+        blobs_lst = []
+        for blob in blobs:
+            #print(blob.name)
+            blobs_lst.append(blob.name)
+        return blobs_lst    
+
+def loop_check(data):
+        print("data['name']:{}".format(data['name']))
+        if data['name'] == 'logging/heatmap.csv.gz':
+          return True  
+
+#------------------------------------ メソッド -----------------------------------------
+
+def main(data):
+        
+    #ディレクトリ準備
+    print('Dir作成')
+    tmpdir = tempfile.TemporaryDirectory()
+    dir_name = tmpdir.name
+"""
+    #media_idのファイル名リスト作成
+    blobs_lst = list_blobs('heatmap-staging')
+    json_lst = []
+    for blob in blobs_lst:
+        if re.search(r'medium_items/media/[0-9]+/heatmap\.json',blob):
+            json_lst.append(blob)
+"""
+    #blobからjsonファイル取得してヒートマップ作成
+    #for json in json_lst:
+    json = data['name']
+    num = re.search(r'\d+',json)
+    media_id = num.group()
+    bucket_name = 'heatmap-staging'
+    #source_blob_name = 'medium_items/media/' +media_id + '/heatmap.json'
+    source_blob_name = json
+    file_name = dir_name + '/' + media_id + '.json'
+    #blobからjsonファイル取得
+    download_blob(bucket_name,source_blob_name,file_name)
     
+    #ヒートマップ作成
+    heatmap_maker_json(file_name,dir_name)
+    
+    os.remove(file_name) #json削除
 
+    tmpdir.cleanup() #仮想dir削除
 
 if __name__ == '__main__':
-    main() 
+    main()
